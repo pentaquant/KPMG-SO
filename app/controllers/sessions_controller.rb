00c9713ab7e45b9910c881fd7dfef6945689581e
Jay Papisan <email": null>
2017-06-08 15:33:55 -0700

    changed name of session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+get '/sessions' do
+  erb :'/sessions/new'
+end
+
+post '/sessions' do
+  @user = User.find_by(username: params[:username])
+  if @user.authenticate(params[:password])
+    session[:id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    erb :'/sessions/new'
+  end
+end
+
+
+delete '/sessions' do
+  session[:id] = nil
+  redirect '/sessions'
+end

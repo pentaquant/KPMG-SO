894a2d4b3f38868f2b0644d52afb721119bbd6d5
Jay Papisan <email": null>
2017-06-09 11:58:56 -0700

    Add answer controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,29 @@
 get '/questions' do
   @questions = Question.all
+
   erb :'questions/index'
-end
-
-get '/questions/:id' do
-
-  @question = Question.find(params[:id])
-
-  erb :'questions/show'
-
 end
 
 get '/questions/new' do
 
-  erb :'questions/new'
-
+erb :'questions/new'
 end
 
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+
+  erb :'questions/show'
+end
+
+
+
 post '/questions' do
-
-  @question = Question.new(params[:question]) #create new question
-
-  if @question.save #saves new question or returns false if unsuccessful
-    redirect '/questions' #redirect back to questions index page
+  @question = Question.new(params[:post])
+  if @question.save
+    redirect '/questions'
   else
-    erb :'questions/new' # show new questions view again(potentially displaying errors)
+
+    erb :'questions/new'
   end
 
 end
@@ -32,5 +31,6 @@ end
 delete '/questions/:id' do
   @question = Question.find(params[:id])
   @question.destroy
+
   redirect '/questions'
 end

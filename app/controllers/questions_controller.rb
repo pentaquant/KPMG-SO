c3137081a48f2b68a36c42213029419d6555979d
Jay Papisan <email": null>
2017-06-09 15:51:21 -0700

    Add comments controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,18 +4,15 @@ get '/questions' do
   erb :'questions/index'
 end
 
+get '/questions/:id/comment' do
+  @question = Question.find(params[:id])
+  erb :'comments/new'
+end
+
 get '/questions/new' do
 
-erb :'questions/new'
+  erb :'questions/new'
 end
-
-get '/questions/:id' do
-  @question = Question.find(params[:id])
-
-  erb :'questions/show'
-end
-
-
 
 post '/questions' do
   @question = Question.new(params[:post])
@@ -24,6 +21,24 @@ post '/questions' do
   else
 
     erb :'questions/new'
+  end
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+
+  erb :'questions/show'
+end
+
+post '/questions/comments' do
+  @question = Question.find(params[:id])
+  @question.comments << Comment.new(params[post[:comment]])
+  if @question.comments.save
+
+    redirect "/questions/#{@question.id}"
+  else
+
+    erb :'comments/new'
   end
 
 end
